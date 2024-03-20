1fc854dbe08b4d7a77b50852c5bf9728919dc5cb
Tait-Ruddell <21190@fraser.school.nz>
2024-03-21 12:18:59 +1300

    firstcommit

---
+++ yes no.py
@@ -0,0 +1,17 @@
+#Functions go here
+def yes_no(question):
+
+#main routine goes here
+
+while True:    
+    want_instructions = input(" Do you want to read the" "instructions ? ").lower()
+    
+
+    if want_instructions == "yes" or want_instructions == "y":
+        print("instructions go here ")
+    elif want_instructions == "no" or want_instructions == "n":
+        pass
+    else:
+        print("please answer yes / no ")
+
+print(" we are done")
